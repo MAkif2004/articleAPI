a2b9686fab21e0d097806e4f2e6d882e603252ef
MAkif2004 <akif.golge@gmail.com>
2023-04-08 20:09:57 +0200

    updated

--- articleAPI/database/seeders/ArticlesTableSeeder.php
+++ articleAPI/database/seeders/ArticlesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class ArticlesTableSeeder extends Seeder
@@ -12,6 +13,23 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++)
+        {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph
+            ]);
+        }
+
+
+
+
+
+
+
     }
 }
